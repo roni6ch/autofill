6888ebb855dfeb80d158a735ca1a176d94004ca3
Roni <roni691986@gmail.com>
2019-07-11 16:56:43 +0300

    init

---
+++ content/data/data.js
@@ -0,0 +1,71 @@
+var formInputsObj = {
+    info: [
+        {
+            name: 'Name',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Middle',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Family',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Phone',
+            type: 'number',
+            icon: 'phone'
+        },
+        {
+            name: 'Mail',
+            type: 'email',
+            icon: 'mail'
+        },
+        {
+            name: 'Street',
+            type: 'text',
+            icon: 'map'
+        },
+        {
+            name: 'City',
+            type: 'text',
+            icon: 'map'
+        },
+        {
+            name: 'Zip',
+            type: 'number',
+            icon: 'map'
+        },
+
+    ],
+    social: [
+        {
+            name: 'LinkedIn',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Facebook',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Git',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Website',
+            type: 'text',
+            icon: 'person'
+        },
+        {
+            name: 'Other',
+            type: 'text',
+            icon: 'person'
+        }]
+}
---
+++ content/js/site.js
@@ -0,0 +1,51 @@
+let formInfo = '', social = '';
+for (i = 0; i < formInputsObj.info.length; i++) {
+    let obj = formInputsObj.info[i];
+    formInfo += `<div class="col-4">
+            <div class="row">
+                <div class="input-field col s12">
+                <i class="material-icons prefix">${obj.icon}</i>
+                <input type="${obj.type}" id="${obj.name}" name="${obj.name}" class="autocomplete">
+                <label for="${obj.name}">${obj.name}</label>
+            </div>
+        </div>
+    </div>`;
+}
+for (i = 0; i < formInputsObj.social.length; i++) {
+    let obj = formInputsObj.social[i];
+    social += `<div class="col-4">
+    <div class="row">
+        <div class="input-field col s12">
+        <i class="material-icons prefix">${obj.icon}</i>
+        <input type="${obj.type}" id="${obj.name}" name="${obj.name}" class="autocomplete">
+        <label for="${obj.name}">${obj.name}</label>
+    </div>
+        </div>
+        </div>`;
+}
+$('.profile').append(formInfo);
+$('.social').append(social);
+
+$("#autoFillForm").submit(function (e) {
+    e.preventDefault();
+    let autoFill = $("#autoFillForm").serializeArray();
+    console.log(autoFill);
+    localStorage.setItem('autofill', JSON.stringify(autoFill));
+   // chrome.storage.sync.set({"autofill":JSON.stringify(autoFill)});
+
+chrome.storage.sync.set({ "autofill": JSON.stringify(autoFill) }, function(){
+    //  A data saved callback omg so fancy
+});
+
+});
+$("#fill").click(function (e) {
+    e.preventDefault();
+    let autoFill = JSON.parse(localStorage.getItem('autofill'));
+    $( document.querySelectorAll("input") ).each(function( i,val ) {
+        let filterObj = _.filter(autoFill, function(o) { return o.name.toLowerCase() === val.name.toLowerCase(); });
+        if (filterObj.length > 0){
+              $(this).val(filterObj[0].value);
+              $(this).parent().find('label').addClass('active');
+          }
+      })
+});
